e9fd75530b4934a3fe807a90a1f85498925f1445
Marcelo Reis <marcelobreis12345@gmail.com>
2025-11-04 03:00:36 -0300

    feat: adicionar método de adicionar itens ao carrinho

--- js/cart-utils.js
+++ js/cart-utils.js
@@ -7,5 +7,27 @@ const CartService = {
     
     setCart(cart) {
         localStorage.setItem(this.STORAGE_KEY, JSON.stringify(cart));
+    },
+    
+    addItem(product, quantity = 1) {
+        const cart = this.getCart();
+        const existingItem = cart.find(item => item.id === product.id);
+        
+        if (existingItem) {
+            existingItem.quantity += quantity;
+        } else {
+            cart.push({
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                oldPrice: product.oldPrice,
+                image: product.image,
+                rating: product.rating,
+                ratingCount: product.ratingCount,
+                quantity: quantity
+            });
+        }
+        
+        this.setCart(cart);
     }
 };
