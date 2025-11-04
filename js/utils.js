b67f8e0b72094ff40119d71fa9778ec29bf79803
Marcelo Reis <marcelobreis12345@gmail.com>
2025-11-04 03:00:33 -0300

    feat: adicionar função para criar cards de produtos

--- js/utils.js
+++ js/utils.js
@@ -18,3 +18,34 @@ const ProductService = {
         this.cache = null;
     }
 };
+
+function createProductCard(product) {
+    const article = document.createElement('article');
+    article.className = 'product-card';
+    article.dataset.id = product.id;
+    
+    article.innerHTML = `
+        <img src="${product.image}" alt="${product.name}" class="product-image">
+        <div class="discount-tag">-${product.discount}%</div>
+        <h3 class="product-title">${product.name}</h3>
+        <div class="product-rating">
+            <img src="images/avaliacao.png" alt="5 estrelas">
+            <span class="rating-count">(${product.ratingCount})</span>
+        </div>
+        <div class="price-initial">
+            <span class="price-label-de">De</span>
+            <span class="price-value-old">R$${product.oldPrice.toFixed(2)}</span>
+            <span class="price-label-por">por</span>
+        </div>
+        <div class="price-final">
+            <span class="price-currency">R$</span>
+            <span class="price-value">${product.price.toFixed(2)}</span>
+        </div>
+    `;
+    
+    article.addEventListener('click', () => {
+        window.location.href = `product-detail.html?id=${product.id}`;
+    });
+    
+    return article;
+}
