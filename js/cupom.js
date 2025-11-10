01eb74ad6a28909ee29ece5f1c3706f84692ec6e
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2025-11-10 19:38:48 -0300

    feat: captura dos feedbacks

--- js/cupom.js
+++ js/cupom.js
@@ -1,9 +1,11 @@
 // Esse JS vai lidar com validação de cupons de desconto!
 
-const okCupomButton = document.getElementById("ok-cupom")
-const inputCupom = document.getElementById("cupom-desconto")
+const okCupomButton = document.getElementById("ok-cupom");
+const inputCupom = document.getElementById("cupom-desconto");
+const feedbackPositivo = document.getElementById("feedback-positivo");
+const feedbackNegativo = document.getElementById("feedback-negativo")
 
-okCupomButton.addEventListener("click",()=>{
-   const valorInputCupom = inputCupom.value;
-   console.log(valorInputCupom)
-})
+okCupomButton.addEventListener("click", () => {
+    const valorInputCupom = inputCupom.value;
+    console.log(valorInputCupom);
+});
