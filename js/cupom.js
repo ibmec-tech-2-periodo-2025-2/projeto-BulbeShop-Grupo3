82bb0470571fe78c7e5cb4d5e802da541fb4a623
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2025-11-10 19:55:51 -0300

    feat: criação da primeira versão do banco de cupons

--- js/cupom.js
+++ js/cupom.js
@@ -3,7 +3,16 @@
 const okCupomButton = document.getElementById("ok-cupom");
 const inputCupom = document.getElementById("cupom-desconto");
 const feedbackPositivo = document.getElementById("feedback-positivo");
-const feedbackNegativo = document.getElementById("feedback-negativo")
+const feedbackNegativo = document.getElementById("feedback-negativo");
+
+// Banco de cupons
+const cupons = [
+    { codigo: "BULBE10", valor: 0.1 },
+    { codigo: "BULBE15", valor: 0.15 },
+    { codigo: "BLACKFRIDAY", valor: 0.2 },
+];
+
+let desconto = 0;
 
 okCupomButton.addEventListener("click", () => {
     const valorInputCupom = inputCupom.value;
