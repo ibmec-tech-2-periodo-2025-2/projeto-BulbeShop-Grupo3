ae15160a386c90fa83c5f574e99312eeaa85688d
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2025-11-10 19:13:26 -0300

    feat: captura do valor do input

--- js/cupom.js
+++ js/cupom.js
@@ -1,3 +1,9 @@
 // Esse JS vai lidar com validação de cupons de desconto!
 
 const okCupomButton = document.getElementById("ok-cupom")
+const inputCupom = document.getElementById("cupom-desconto")
+
+okCupomButton.addEventListener("click",()=>{
+   const valorInputCupom = inputCupom.value;
+   console.log(valorInputCupom)
+})
