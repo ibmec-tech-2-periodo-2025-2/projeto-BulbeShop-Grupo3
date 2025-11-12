14c1e4d8fb072ef74ca4aa9314dd2687356d855d
augustolobo18 <augustolobo18@gmail.com>
2025-11-12 15:47:40 -0300

    feat: java script para voltar o menu lateral, retirando a classe .active

--- js/menu-sanduiche.js
+++ js/menu-sanduiche.js
@@ -1,6 +1,11 @@
 const btnSanduiche = document.getElementsByClassName("menu-btn")[0];
 const menu = document.getElementById("sidebar-menu");
+const btnFecharMenu = document.getElementById("close-btn");
 
 btnSanduiche.addEventListener("click",() => {
     menu.classList.toggle("active");
+})
+
+btnFecharMenu.addEventListener("click",() => {
+    menu.classList.remove("active");
 })
